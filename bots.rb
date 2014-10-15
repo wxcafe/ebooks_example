7c1b335db2e8ed237b5dbf121028b61f2c30f4b9
Wxcafé (Clément Hertling) <wxcafe@wxcafe.net>
2014-10-15 17:47:01 +0200

    fixes dependency problem with update of simple_oauth, stops the bots from faving all tweets containing 'bot' (spammers...)

--- bots.rb
+++ bots.rb
@@ -14,7 +14,7 @@ TEXT_MODEL_NAME = "example" # This should be the name of the text model
 
 DELAY = 2..10 # Simulated human reply delay range in seconds
 BLACKLIST = ['horse_ebooks'] # Users to avoid interaction with
-SPECIAL_WORDS = ['ebooks', 'bot', 'bots', 'clone', 'singularity', 'world domination']
+SPECIAL_WORDS = ['ebooks', 'clone', 'singularity', 'world domination']
 
 # Track who we've randomly interacted with globally
 $have_talked = {}
